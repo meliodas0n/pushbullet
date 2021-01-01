cc2f25452740ff13cd7878a8bc804494e9957c52
Mrunal Narayana <37010807+mrunal0n@users.noreply.github.com>
2021-01-02 01:14:47 +0530

    first step completed

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 from pushbullet import Pushbullet
+import datetime
+import multiprocessing as mp
+import time
+import sys
+import signal
+import os
 
 PB_API_KEY = 'o.hqwwAkrFglblWfL0Q4slIyRCLtZ50cea'
+pb = Pushbullet(PB_API_KEY)
 
-print("creating pb object with key: ")
+def process(hr, minute):
+    while True:
+        d = datetime.datetime.now()
+        if d.hour == hr and d.minute == minute:
+            push = pb.push_note("this a test", "this is a test with specific time")
+            os.kill(os.getppid(), signal.SIGTERM)
+            sys.exit()
+        else:
+            time.sleep(1)
 
-try:
-    pb = Pushbullet(PB_API_KEY)
-except Exception as e:
-    print(str(e))
-    exit()
-
-print("pushing note: ")
-
-try:
-    push = pb.push_note('important subject', 'this is a test')
-except Exception as e:
-    print(str(e))
-    exit()
-
-print("done")
+if __name__ == '__main__':
+    p = mp.Process(target = process, args = (1, 14))
+    p.start()
